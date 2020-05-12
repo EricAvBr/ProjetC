f318a62bf5a9643f96c78bab7115bd94dfdefa2c
EricBrt <65215589+EricBrt@users.noreply.github.com>
2020-05-12 09:09:16 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#import <stdio.h>
+
+
+
+//On a : 4 fonctions.
+//VerifierAlphanumérique -> Renvoie un message d'erreur si il y a un caractère alphanumérique
+
+//ConvertirAccents -> Convertis les caractères alphanumériques (É, é, à, ù, etc) en caractère "simple"
+
+//
