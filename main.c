344902354ef975405d09690b8cb39b463de5de08
EricBrt <65215589+EricBrt@users.noreply.github.com>
2020-05-12 09:12:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #import <stdio.h>
 
+//On a : 4 fonctions :
+//VerifierAlphanumérique -> Renvoie un message d'erreur si il y a un caractère alphanumérique.
+//ConvertirAccents -> Convertis les caractères alphanumériques (É, é, à, ù, etc) en caractère "simple".
+//Chiffre -> Chiffre le texte avec le code césar.
+//Déchiffre -> Déchiffre le texte avec le code césar.
 
 
-//On a : 4 fonctions.
-//VerifierAlphanumérique -> Renvoie un message d'erreur si il y a un caractère alphanumérique
-
-//ConvertirAccents -> Convertis les caractères alphanumériques (É, é, à, ù, etc) en caractère "simple"
-
-//
