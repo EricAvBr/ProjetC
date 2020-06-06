39fd876e5a1ba2930dfe91850b5cd5982a375fcb
EricBrt <65215589+EricBrt@users.noreply.github.com>
2020-06-06 03:31:45 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,50 +1,115 @@
-#import <stdio.h>
-#define CHIFFREMENT_CLEF 4
-
-//On a : 4 fonctions :
-//VerifierAlphanumérique -> Renvoie un message d'erreur si il y a un caractère alphanumérique.
-//ConvertirAccents -> Convertis les caractères alphanumériques (É, é, à, ù, etc) en caractère "simple".
-//Chiffre -> Chiffre le texte avec le code césar.
-//Déchiffre -> Déchiffre le texte avec le code césar.
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <string.h>
 
 
-void Chiffrement (char rep[], char chaineChiffree[]){
-  char chaine[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char chaineMini[26] = "abcdefghijklmnopqrstuvwxyz";
-  int a;
-  for(i=0, i<strlen(rep))
-    if(rep[i]=chaine[a])
-      chaineChiffree[i]=chaine[a+CHIFFREMENT_CLEF]%26;
-      i++;
-    else
-       if(rep[i])=chaineMini[a])
-         rep[i]=chaineMini[a+CHIFFREMENT_CLEF]%26;
-         i++;
-       else
-       a++;
+// ---------------------- A FAIRE -----------------------
+void chiffrement(wchar_t* texte, wchar_t* texteChiffre, int* clef){  //A FAIRE
+	char alphabet[26]= "abcdefghijklmnopqrstuvwxyz";
+	int a;
+	int i;
+	texteChiffre=texte;
+	for(i=0; i<wcslen(texte); i++)
+	{
+		for(a=0; a<strlen(alphabet); a++)
+		{
+			if(texte[a]=alphabet[i])
+			{
+				texteChiffre[a]=alphabet[i+2];
+			}
+		}
+	}	
 }
-  
-  
-void Dechiffrement (char rep[], char chaineChiffree[]){
-  char chaine[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char chaineMini[26] = "abcdefghijklmnopqrstuvwxyz";
-  int a;
-  for(i=0, i<strlen(chaineChiffree))
-    if(chaineChiffree[i]=chaine[a])
-      rep[i]=chaine[a-CHIFFREMENT_CLEF]%26;
-      i++;
-    else
-      if(chaineChiffree[i]=chaineMini[a])
-        rep[i]=chaineMini[a-CHIFFREMENT_CLEF]%26;
-        i++;
-      else
-      a++;
+// ------------------- A FAIRE ------------------------------
+void dechiffrement(){}                                                   
+
+
+// ------------------- FONCTIONNEL ---------------------------
+void convertirAccents(wchar_t* texte){
+	int i;
+	for(i=0; i<wcslen(texte); i++)
+	{
+		switch(texte[i]){
+			case L'É': case L'é': case L'È': case L'è': case L'Ê': case L'ê': case L'Ë': case L'ë':
+			texte[i]=L'e';
+			break;
+			case L'Á': case L'á': case L'À': case L'à': case L'Â': case L'â': case L'Ä': case L'ä':
+			texte[i]=L'a';
+			break;
+			case L'Ç': case L'ç':
+			texte[i]=L'c';
+			break;
+			case L'Î': case L'î': case L'Ï': case L'ï':
+			texte[i]=L'i';
+			break;
+			case L'Ô': case L'ô':
+			texte[i]=L'o';
+			break;
+			case L'Ù': case L'ù': case L'Û': case L'û': case L'Ü': case L'ü':
+			texte[i]=L'u';
+			break;
+		default:
+		texte[i]=texte[i];
+	}
+	}
 }
 
-void ConvertirAccents(){}
+// ------------------------ FONCTIONNEL ------------------------
+int verifierAlphanumerique(wchar_t* texte){
+	wchar_t caracAlpha[42]=L"><#({[]})*-+.@|&~_`^?;,:!%$§µ£¤*€=°²\'\\/";
+	int i;
+	int a;
+	for(i=0; i<wcslen(texte); i++)
+	{
+		for(a=0; a<wcslen(caracAlpha); a++)
+		{
+			if(texte[i]==caracAlpha[a])
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+// ------------------ FONCTIONNEL ----------------------
+void viderBuffer(){
+	int c = 0;
+	while (c != '\n' && c != EOF)
+	{
+	c = getchar();
+	}
+}
 
 
-void VerifierAlphanumerique(){}
+void main(){
+	setlocale(LC_ALL,"fr_FR.UTF-8");
+	wchar_t texte[200];
+	wchar_t texteChiffre[200];
+	int alphanum;
+	int* clef;
+	wprintf(L"---- Quel est le texte que vous voulez chiffer ? ----\n");
+	fgetws(texte, 200, stdin);
+	wprintf(L"---- Quelle clef de chiffrement souhaitez-vous ? Elle doit être comprise entre 0 et 25. ----\n");
+	wscanf(L"%d", clef);
+	alphanum = verifierAlphanumerique(texte);
+	while(alphanum==1)
+	{
+		wprintf(L"---- Votre message ne doit pas contenir de caractères spéciaux. ----\n");
+		wprintf(L"---- Quel est le message que vous voulez chiffrer ? ----\n");
+		fgetws(texte, 200, stdin);
+		alphanum = verifierAlphanumerique(texte);
+		viderBuffer();
+	}
+	convertirAccents(texte);
+	wprintf(L"---- Le texte converti sans accent : ----\n");
+	wprintf(L"%ls\n", texte);
+	wprintf(L"---- Le texte converti avec la méthode de chiffrement césar : ----\n");
+	chiffrement(texte, texteChiffre, clef);
+	wprintf(L"%ls\n", texteChiffre);
+	viderBuffer();
+}
 
 
-void main(){}
